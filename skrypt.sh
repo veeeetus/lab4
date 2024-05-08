59553588a39d03f4ac32931a50a0966086a50b9f
veeeetus <dawbor589@gmail.com>
2024-05-09 01:46:43 +0200

    krótsza flaga

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-if [[ "$1" == "--date" ]]; then
+display_help() {
+    echo "Użycie: $0 [OPCJA]"
+    echo "Dostępne opcje:"
+    echo "  --date           Wyświetla dzisiejszą datę."
+    echo "  --logs [liczba]  Tworzy podaną liczbę plików logów. Domyślnie 100."
+    echo "  --help, -h       Wyświetla pomoc."
+}
+
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    display_help
+elif [[ "$1" == "--date" ]]; then
     date +"Dzisiaj jest %Y-%m-%d"
+elif [[ "$1" == "--logs" ]]; then
+    if [[ -n "$2" && "$2" =~ ^[0-9]+$ ]]; then
+        num_logs="$2"
+    else
+        num_logs=100
+    fi
+    
+    for ((i=1; i<=$num_logs; i++)); do
+        log_file="log$i.txt"
+        echo "Plik logowy: $log_file" > "$log_file"
+        echo "Utworzony przez: $0" >> "$log_file"
+        echo "Data utworzenia: $(date +"%Y-%m-%d %T")" >> "$log_file"
+    done
 else
-    echo "Użycie: $0 --date"
+    echo "Niepoprawne użycie."
+    display_help
 fi
