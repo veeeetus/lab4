c545cf457ef56ded28401f2a324b048eb332e92b
veeeetus <dawbor589@gmail.com>
2024-05-09 01:36:00 +0200

    logs100

--- skrypt.sh
+++ skrypt.sh
@@ -2,6 +2,15 @@
 
 if [[ "$1" == "--date" ]]; then
     date +"Dzisiaj jest %Y-%m-%d"
+elif [[ "$1" == "--logs" ]]; then
+    for ((i=1; i<=100; i++)); do
+        log_file="log$i.txt"
+        echo "Plik logowy: $log_file" > "$log_file"
+        echo "Utworzony przez: $0" >> "$log_file"
+        echo "Data utworzenia: $(date +"%Y-%m-%d %T")" >> "$log_file"
+    done
 else
     echo "Użycie: $0 --date"
+    echo "lub"
+    echo "Użycie: $0 --logs"
 fi
