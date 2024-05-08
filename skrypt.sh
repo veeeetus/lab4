8e8d695c34964e99e7e9a82393c2ca889172a1a0
veeeetus <dawbor589@gmail.com>
2024-05-09 01:49:58 +0200

    fix2

--- skrypt.sh
+++ skrypt.sh
@@ -3,16 +3,14 @@
 display_help() {
     echo "Użycie: $0 [OPCJA]"
     echo "Dostępne opcje:"
-    echo "  --date           Wyświetla dzisiejszą datę."
-    echo "  --logs [liczba]  Tworzy podaną liczbę plików logów. Domyślnie 100."
-    echo "  --help, -h       Wyświetla pomoc."
+    echo "  -d, --date           Wyświetla dzisiejszą datę."
+    echo "  -l, --logs [liczba]  Tworzy podaną liczbę plików logów. Domyślnie 100."
+    echo "  -h, --help           Wyświetla pomoc."
 }
 
-if [[ "$1" == "--help" || "$1" == "-h" ]]; then
-    display_help
-elif [[ "$1" == "--date" ]]; then
+if [[ "$1" == "--date" || "$1" == "-d" ]]; then
     date +"Dzisiaj jest %Y-%m-%d"
-elif [[ "$1" == "--logs" ]]; then
+elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
     if [[ -n "$2" && "$2" =~ ^[0-9]+$ ]]; then
         num_logs="$2"
     else
@@ -25,37 +23,7 @@ elif [[ "$1" == "--logs" ]]; then
         echo "Utworzony przez: $0" >> "$log_file"
         echo "Data utworzenia: $(date +"%Y-%m-%d %T")" >> "$log_file"
     done
-else
-    echo "Niepoprawne użycie."
-    display_help
-fi
-
-#!/bin/bash
-
-display_help() {
-    echo "Użycie: $0 [OPCJA]"
-    echo "Dostępne opcje:"
-    echo "  --date           Wyświetla dzisiejszą datę."
-    echo "  --logs [liczba]  Tworzy podaną liczbę plików logów. Domyślnie 100."
-    echo "  --help           Wyświetla pomoc."
-}
-
-if [[ "$1" == "--date" ]]; then
-    date +"Dzisiaj jest %Y-%m-%d"
-elif [[ "$1" == "--logs" ]]; then
-    if [[ -n "$2" && "$2" =~ ^[0-9]+$ ]]; then
-        num_logs="$2"
-    else
-        num_logs=100
-    fi
-    
-    for ((i=1; i<=$num_logs; i++)); do
-        log_file="log$i.txt"
-        echo "Plik logowy: $log_file" > "$log_file"
-        echo "Utworzony przez: $0" >> "$log_file"
-        echo "Data utworzenia: $(date +"%Y-%m-%d %T")" >> "$log_file"
-    done
-elif [[ "$1" == "--help" ]]; then
+elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
     display_help
 else
     echo "Niepoprawne użycie."
