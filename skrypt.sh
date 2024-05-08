8b4769c0b1d42d4b9db909972395fdef3ee2b49a
veeeetus <dawbor589@gmail.com>
2024-05-09 01:53:30 +0200

    init

--- skrypt.sh
+++ skrypt.sh
@@ -3,14 +3,17 @@
 display_help() {
     echo "Użycie: $0 [OPCJA]"
     echo "Dostępne opcje:"
-    echo "  --date, -d           Wyświetla dzisiejszą datę."
-    echo "  --logs, -l [liczba]  Tworzy podaną liczbę plików logów. Domyślnie 100."
-    echo "  --help, -h           Wyświetla pomoc."
+    echo "  --date           Wyświetla dzisiejszą datę."
+    echo "  --logs [liczba]  Tworzy podaną liczbę plików logów. Domyślnie 100."
+    echo "  --init           Klonuje całe repozytorium do katalogu bieżącego i ustawia ścieżkę w zmiennej PATH."
+    echo "  --help, -h       Wyświetla pomoc."
 }
 
-if [[ "$1" == "--date" || "$1" == "-d" ]]; then
+if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    display_help
+elif [[ "$1" == "--date" ]]; then
     date +"Dzisiaj jest %Y-%m-%d"
-elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
+elif [[ "$1" == "--logs" ]]; then
     if [[ -n "$2" && "$2" =~ ^[0-9]+$ ]]; then
         num_logs="$2"
     else
@@ -23,8 +26,14 @@ elif [[ "$1" == "--logs" || "$1" == "-l" ]]; then
         echo "Utworzony przez: $0" >> "$log_file"
         echo "Data utworzenia: $(date +"%Y-%m-%d %T")" >> "$log_file"
     done
-elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
-    display_help
+elif [[ "$1" == "--init" ]]; then
+    # Pobieranie bieżącej ścieżki
+    current_path=$(pwd)
+    # Klonowanie repozytorium do bieżącego katalogu
+    git clone https://example.com/repo.git "$current_path/repo"
+    # Dodanie katalogu z repozytorium do zmiennej środowiskowej PATH
+    echo "export PATH=\"$current_path/repo:\$PATH\"" >> ~/.bashrc
+    echo "Zaktualizowano zmienną środowiskową PATH. Zrestartuj terminal, aby wprowadzić zmiany."
 else
     echo "Niepoprawne użycie."
     display_help
